237ca3e9d5ef6a82eb5305d14dd3b7423d9cf8a7
Ivan <cean.developmentbg@gmail.com>
2025-05-22 16:24:10 +0300

    osnovi front end
    thaiwing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'; // <-- Важно: Използва се 'import', а не 'require'
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173, // Или какъвто порт си избрал
+    // host: true, // Ако си добавил това, остави го
+  },
+});
